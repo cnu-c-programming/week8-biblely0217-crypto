cc7c4a3469aca83a6d97a41cac7424c08a29f95a
bible <biblely0217@gmail.com>
2026-04-24 09:28:21 +0900

    solve 3-2

---
+++ code/3-1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char *arr[] = {"banana", "apple", "cat", "anaconda"};
+    int n = 4;
+
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (strcmp(arr[j], arr[j + 1]) > 0) {
+                char *temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        printf("%s\n", arr[i]);
+    }
+
+    return 0;
+}
